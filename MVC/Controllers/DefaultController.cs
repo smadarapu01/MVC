e0e260e7c958b873d016ae37f588c9b237534fbf
Srinivas Madarapu <sri.nivas006@gmail.com>
2021-01-30 13:35:13 +0530

    ViewBag & ViewData Examples

---
+++ MVC/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewData["Countries"] = new List<string>()
+            {
+                "India",
+                "USA",
+                "UK",
+                "China",
+                "Canada"
+            };
+
+            return View();
+        }
+    }
+}
