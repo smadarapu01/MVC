b69b0818c2c01cc4577a811d296600d2e0692061
Srinivas Madarapu <sri.nivas006@gmail.com>
2021-02-02 10:12:07 +0530

    HTMLHelper Examples For

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAccess;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
@@ -10,7 +12,34 @@ namespace MVC.Controllers
     {
         public ActionResult Index()
         {
+            EmployeeDBEntities dbContext = new EmployeeDBEntities();
+
+            List<SelectListItem> selectList = new List<SelectListItem>();
+
+            foreach (var item in dbContext.tblDepartments.ToList())
+            {
+                SelectListItem selectListItem = new SelectListItem
+                {
+                    Text = item.Name,
+                    Value = item.Name,
+                };
+
+                selectList.Add(selectListItem);
+            }
+
+            ViewBag.Departments = selectList;
+
+            //ViewBag.Departments = new SelectList(dbContext.tblDepartments.ToList(), "Id", "Name", "2");
+
             return View();
+        }
+
+
+        public ActionResult Index1()
+        {
+            Company company = new Company("SriniTech");
+
+            return View(company);
         }
 
         public ActionResult About()
---
+++ MVC/Models/Company.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Company
+    {
+		private string _companyName;
+	
+		public Company(string companyName)
+		{
+			_companyName = companyName;
+		}
+
+		public List<Department> Departments 
+		{
+			get
+			{
+				List<Department> departments = new List<Department>();
+				EmployeeDBEntities db = new EmployeeDBEntities();
+				foreach (var item in db.tblDepartments.ToList())
+				{
+					departments.Add(TransformDeptToModel(item));
+				}
+				return departments;
+			}
+		}
+
+
+
+		public string companyName
+		{
+			get { return _companyName; }
+			set { _companyName = value; }
+		}
+
+		private static Models.Department TransformDeptToModel(DataAccess.tblDepartment dDept)
+		{
+			Models.Department dept = new Models.Department();
+
+			dept.DeptId = dDept.Id;
+			dept.DeptName = dDept.Name;
+
+			return dept;
+		}
+
+	}
+}
